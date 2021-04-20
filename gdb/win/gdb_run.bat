3a9d3ae6cc1aaf97d25d49949921d48435575909
Alex Olkhovsky <>
2021-04-20 10:50:43 +0600

    Поправил gdb файлы для винды, зависает в бесконечном цикле (хэндлер на ватчдог?).

---
+++ gdb/win/gdb_run.bat
@@ -0,0 +1,3 @@
+@REM arm-none-eabi-gdb -x C:\STM32Toolchain\projects\disc_i2c\_la\scr.gdbs
+
+arm-none-eabi-gdb -x C:\_work\stm32f4_\stm32f4\gdb\win\scr.gdbs 
---
+++ gdb/win/ocd_run.bat
@@ -0,0 +1,4 @@
+@REM cd C:\STM32Toolchain\openocd\scripts
+@REM ..\bin\openocd.exe -f \board\st_nucleo_f4.cfg
+
+C:\STM32Toolchain\openocd\bin\openocd.exe -f C:\STM32Toolchain\openocd\scripts\board\st_nucleo_f4.cfg
